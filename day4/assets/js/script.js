684312e42dcd6d0d88b2d888e1a64b307d6b479f
ledinhhieu22122002 <ledinhhieu1212003@gmail.com>
2022-08-08 14:55:13 +0700

    up load 2

--- day4/assets/js/script.js
+++ day4/assets/js/script.js
@@ -7,16 +7,61 @@ const btnNext = $('.next');
 const images = $$('.image img');
 const galleryImg = $('.gallery__inner img');
 const gallery = $('.gallery');
-const currenIndex = 0
-images.forEach((element, index) => {
-    element.onclick = () => {
-        currenIndex = index;
-        galleryImg.src = images[currenIndex].src;
-        gallery.classList.add('show');
+let currentIndex = 0;
+const lengthImg = images.length;
+
+function galleryShow() {
+    if (currentIndex == 0) {
+        btnPrev.classList.add('hide');
+    } else {
+        btnPrev.classList.remove('hide');
     }
+    if (currentIndex == lengthImg - 1) {
+        btnNext.classList.add('hide');
+    } else {
+        btnNext.classList.remove('hide');
+    }
+
+    galleryImg.src = images[currentIndex].src;
+    gallery.classList.add('show');
+}
+
+images.forEach((element, index) => {
+    element.addEventListener("click", () => {
+        currentIndex = index;
+        galleryShow();
+    })
 });
 
 
-close.addEventListener('click', () => {
-    
+btnClose.addEventListener('click', () => {
+    gallery.classList.remove('show');
+})
+
+document.addEventListener('keydown', (event) => {
+    var e = event || window.event;
+    if (e.keyCode == 27) {
+        gallery.classList.remove('show');
+    }
+    if (e.keyCode == 37 && currentIndex > 0) { //LEFT
+        currentIndex--;
+        galleryShow();
+    } else if (e.keyCode == 39 && currentIndex < 7) { //RIGHT
+        currentIndex++;
+        galleryShow();
+    }
+})
+
+btnPrev.addEventListener('click', () => {
+    if (currentIndex > 0) {
+        currentIndex--;
+        galleryShow();
+    }
+})
+
+btnNext.addEventListener('click', () => {
+    if (currentIndex < lengthImg - 1) {
+        currentIndex++;
+        galleryShow();
+    }
 })
